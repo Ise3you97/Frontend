fe0b806f639fd57c05494fd1bb30c4b4a735647b
Ise3you97 <kelvinsolano566@gmail.com>
2024-07-25 20:53:14 -0500

    Fix Links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Navigation extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="/">Navbar</a>
+          <Link className="navbar-brand" to="/">Navbar</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link  text-right" href="/">Notes</a>
+                <Link className="nav-link text-right" to="/">Notes</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link text-right" href="/create">Create Note</a>
+                <Link className="nav-link text-right" to="/create">Create Note</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link text-right" href="/user">Create User</a>
+                <Link className="nav-link text-right" to="/user">Create User</Link>
               </li>
             </ul>
           </div>
